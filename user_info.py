2621db13be4f38b01a1d9f817d9ed6f8a3a41ef3
Jonathan <vark@uw.edu>
2012-05-17 17:06:59 +0000

    Adding functionality of user info file

---
+++ user_info.py
@@ -0,0 +1,9 @@
+#This class reads in user defined settings for AWS access
+#Jonathan Varkovitzky
+#May 17, 2012
+
+class user_info:
+    def __init__():
+        self.pem_file = 'research'
+        self.security_group = 'sshHTTPallowed'
+        
