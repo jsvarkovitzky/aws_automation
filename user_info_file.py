9330512c2a5373c726f456c546cfb25ab8ffd9ee
Jonathan <vark@uw.edu>
2012-05-18 17:09:54 +0000

    adding user_info on master

---
+++ user_info_file.py
@@ -0,0 +1,11 @@
+#This class reads in user defined settings for AWS access
+#Jonathan Varkovitzky
+#May 17, 2012
+
+class user_info:
+    def __init__(self):
+        #put the *.pem file you wish to use, without the .pem extension
+        self.pem_file = 'research'
+        #put the name of the secruity group you want to use (must be already setup on AWS)
+        self.security_group = 'sshHTTPallowed'
+        
