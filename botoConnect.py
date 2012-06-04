c8004406cfab3d7bd1019a879f8b5de1626ef195
Jonathan <vark@uw.edu>
2012-06-04 22:19:42 +0000

    backing up automation codes

--- botoConnect.py
+++ botoConnect.py
@@ -13,6 +13,8 @@ import time
 import keys
 import sys
 import user_info_file
+import csv
+
 user = user_info_file.user_info()
 
 ################                                                                
@@ -22,9 +24,59 @@ user = user_info_file.user_info()
 def status(n,N):
     n = float(n)
     N = float(N)
+    
     percent = n/N*100
     sys.stdout.write("[==> ]%3d%%\r" %percent)
     sys.stdout.flush()
+
+####################
+## Start Instance ##
+####################
+
+def start_instance():
+    print "The pem key to be used is: %s" %user.pem_file
+#Start a new instance
+    if 1:
+        conn.run_instances('ami-9216b3fb',key_name=user.pem_file,instance_type='m1.large',security_groups=[user.security_group])
+    #Brief pause to wait for instance to start up
+        pause_time = 60
+        print "Pausing for %s seconds to allow instance to turn on..."%pause_time
+        for i in range(0,pause_time):
+            time.sleep(1)
+            status(i,pause_time)
+
+#Retrieve information for connection
+        reservations = conn.get_all_instances()
+        instance = reservations[-1].instances[-1]
+        dns = str(instance.public_dns_name)
+
+        print "Instance_id:"
+        print(instance)
+        print "DNS name:"
+        print(dns)
+
+
+        print "Uploading files to instance..."
+        
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(key_file) + ' ubuntu@' + repr(dns) + ':.')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(script) + ' ubuntu@' + repr(dns) + ':.')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(driver_file) + ' ubuntu@' + repr(dns) + ':.')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(user_info_file) + ' ubuntu@' + repr(dns) + ':.')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(topo_list) + ' ubuntu@' + repr(dns) + ':topo_list.csv')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(georegion_list) + ' ubuntu@' + repr(dns) + ':georegion_list.csv')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(tidegauge_list) + ' ubuntu@' + repr(dns) + ':tidegauge_list.csv')
+        os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(fixedgrid_list) + ' ubuntu@' + repr(dns) + ':fixedgrid_list.csv')
+        print "Executing shell script remotely..."
+        command = 'ssh -i ' + str(user.pem_file) + '.pem ubuntu@' + str(dns) + " ' . " + str(script) + " &'"
+#print command
+        os.system(command)
+        
+        print "*********************************************"
+        print "** You are now back on the driver instance **"
+        print "*********************************************"
+
+
+
 
 ##################
 ## Main Program ##
@@ -32,49 +84,21 @@ def status(n,N):
 
 #Initialize connection with EC2 using personal keys
 
+
+
 conn = EC2Connection(keys.aws_key('access'),keys.aws_key('secret'))
 
-print "The pem key to be used is: %s" %user.pem_file
-#Start a new instance
-if 1:
-    conn.run_instances('ami-9216b3fb',key_name=user.pem_file,instance_type='m1.large',security_groups=[user.security_group])
-    #Brief pause to wait for instance to start up
-    pause_time = 60
-    print "Pausing for %s seconds to allow instance to turn on..."%pause_time
-    for i in range(0,pause_time):
-        time.sleep(1)
-        status(i,pause_time)
-
-#Retrieve information for connection
-reservations = conn.get_all_instances()
-instance = reservations[-1].instances[-1]
-dns = str(instance.public_dns_name)
-
-print "Instance_id:"
-print(instance)
-print "DNS name:"
-print(dns)
-
-script = 'automateTestCase.sh'
-key_file = 'keys.py'
 driver_file = 'Driver.csv'
-user_info_file = 'user_info_file.py'
-topo_list = 'topo_list.csv'
-#Test by scp-ing a text file to aws
-print "Uploading files to instance..."
 
-os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(key_file) + ' ubuntu@' + repr(dns) + ':.')
-os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(script) + ' ubuntu@' + repr(dns) + ':.')
-os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(driver_file) + ' ubuntu@' + repr(dns) + ':.')
-os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(user_info_file) + ' ubuntu@' + repr(dns) + ':.')
-os.system('scp -i ' + repr(user.pem_file) + '.pem -o StrictHostKeyChecking=no ' + repr(topo_list) + ' ubuntu@' + repr(dns) + ':.')
+driver_block = genfromtxt(driver_file, dtype=None, delimiter=',', names=True)
 
-print "Executing shell script remotely..."
-command = 'ssh -i ' + str(user.pem_file) + '.pem ubuntu@' + str(dns) + " ' . " + str(script) + " &'"
-#print command
-os.system(command)
-
-print "*********************************************"
-print "** You are now back on the driver instance **"
-print "*********************************************"
-
+for row in driver_block:
+    script = 'automateTestCase.sh'
+    key_file = 'keys.py'
+    user_info_file = 'user_info_file.py'
+    topo_list = row[8]
+    georegion_list = row[9]
+    tidegauge_list = row[10]
+    fixedgrid_list = row[11]
+    start_instance()
+    
