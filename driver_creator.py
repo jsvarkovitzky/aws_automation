9c812f11d060ee986b373b8f788a32a7017bc22c
Jonathan <vark@uw.edu>
2012-06-08 21:45:07 +0000

    created ability to make unique driver files to upload

---
+++ driver_creator.py
@@ -0,0 +1,19 @@
+def lin_copy(driverMaster,driverSub,row):
+    f = open(driverMaster)
+    # Opening the new file and will overwrite a preexisting version
+    f1 = open(driverSub,'w')
+
+    i = 0
+    for line in f.readlines():
+        if i == 0:
+            print "writing the header to file"
+            f1.write(line)
+        if i == row:
+            print "Copying run number %s to new driver file."%i
+            f1.write(line)
+        else:
+            print "Not copying row %s to file."%i
+        i = i + 1
+    f1.close
+    f.close
+    
